6588af67613da3ab0a312d104bf5742c60865d57
hansebastian <sebastian_hans@hotmail.com>
2021-04-05 02:41:25 +0800

    Add Q24 solution

--- proc.sql
+++ proc.sql
@@ -350,3 +350,33 @@ BEGIN
     AND (date session_date + time end_time) < INTERVAL '0'; -- Course session hasn't started
 END;
 $$ LANGUAGE plpgsql;
+
+/*
+  Q24: Add a new session to a course offering
+*/
+CREATE OR REPLACE PROCEDURE add_session(input_courseId INT, input_launchDate DATE, input_sessionId INT, input_sessionDate DATE,
+                                        input_sessionStart INT, input_instructorId INT, input_roomId INT)
+AS $$
+DECLARE
+    registrationDeadline DATE;
+    endHour INT;
+BEGIN
+    registrationDeadline := (SELECT registration_deadline
+                             FROM CourseOfferings
+                             WHERE course_id = input_courseId;
+                             AND launch_date = input_launchDate);
+    
+    endHour := input_sessionStart + (SELECT duration 
+                                     FROM Courses
+                                     WHERE course_id = input_courseId);
+
+    /*Course offering registration deadline passed*/
+    IF registrationDeadline < CURRENT_DATE THEN
+        RETURN:
+    END IF;
+
+    /*Check session constraints*/
+    INSERT INTO CourseOfferingSessions
+    VALUES (number, input_sessionStart, endHour, input_roomId, input_instructorId, input_courseId, input_sessionDate, input_launchDate);
+END;
+$$ LANGUAGE plpgsql;
